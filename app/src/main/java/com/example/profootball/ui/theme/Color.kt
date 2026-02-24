471967dac016cf208e632c6de5bfc1b90b2ebe6e
kevcorbol04 <kevincortesbolufer2004@gmail.com>
2026-02-25 00:46:49 +0100

    Proyecto creado

---
+++ app/src/main/java/com/example/profootball/ui/theme/Color.kt
@@ -0,0 +1,19 @@
+package com.example.profootball.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BgTop = Color(0xFF0B1220)
+val BgMid = Color(0xFF0E2A3B)
+val Accent = Color(0xFF00E5FF)
+val Accent2 = Color(0xFF7C4DFF)
+
+val TextOnDark = Color(0xFFFFFFFF)
+val ButtonTextDark = Color(0xFF061018)
+
+val GlassBase = Color(0xFFFFFFFF)
+val OutlineOnDark = Color(0xFFFFFFFF)
+
+val Danger = Color(0xFFFF6B6B)
+val Win = Color(0xFF00E676)
+val Draw = Color(0xFFFFD54F)
+val Loss = Color(0xFFFF5252)
